c8b57ec6716bdde471a6a29984949763c217f304
Christian Koh <ckoh.2021@scis.smu.edu.sg>
2025-11-01 21:22:05 +0800

    first commit, adds python-app with kubernetes dpeloytments

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'hostname': socket.gethostname(),
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+    }), 200
+
+@app.route('/api/v1/health')
+def health():
+    return jsonify({
+        'message': 'Health endpoint is working!',
+        'status': 'healthy'
+    }), 200
+
+
+if __name__ == '__main__':
+    # We need '0.0.0.0' to be accessible from outside the container
+    app.run(debug=True, host='0.0.0.0')
